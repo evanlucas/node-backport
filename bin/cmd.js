217c8fa5c73c4d316859aa22ade3e9ba34f61e40
Evan Lucas <evanlucas@me.com>
2017-10-26 13:31:13 -0500

    package: basic cleanup and args parsing

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,8 +2,45 @@
 
 'use strict'
 
+const nopt = require('nopt')
+const help = require('help')()
+const knownOpts = {
+  help: Boolean
+, version: Boolean
+, source: String
+, target: String
+, 'exclude-label': String
+, 'filter-release': Boolean
+}
+const shortHand = {
+  h: ['--help']
+, v: ['--version']
+, s: ['--source']
+, t: ['--target']
+, e: ['--exclude-label']
+}
+const parsed = nopt(knownOpts, shortHand)
+
+if (parsed.help) {
+  return help()
+}
+
+if (parsed.version) {
+  const {version} = require('../package')
+  console.error('node-backport', `v${version}`)
+  return
+}
+
 const BP = require('../')
 
-const bp = new BP()
+const bp = new BP(parsed)
 
-bp.run(console.log)
+bp.run((err) => {
+  if (err) {
+    console.error(err)
+    process.exitCode = 1
+    return
+  }
+
+  console.log('backport complete')
+})
